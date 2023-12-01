76c0743dd9aa3eca5a92e27bf403383c430148b2
micahw210 <113952298+micahw210@users.noreply.github.com>
2023-11-30 23:43:08 -0500

    Add files via upload

--- Error.cshtml.cs
+++ Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace BlazorMicah.Pages
+namespace Blazor_Practive_1.Pages
 {
 	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 	[IgnoreAntiforgeryToken]
